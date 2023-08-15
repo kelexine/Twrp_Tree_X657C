2d637f96c1e95e7550ad53b9cfe0f40b14ea276e
Miraclev1 <fathiralmsyah3@gmail.com>
2023-08-15 21:25:07 +0700

    Twrp_Tree_X657C

---
+++ twrp_X657C.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from spinel device
+$(call inherit-product, device/infinix/X657C/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X657C
+PRODUCT_NAME := omni_X657C
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X657C
+PRODUCT_MANUFACTURER := infinix
+PRODUCT_RELEASE_NAME := Infinix Infinix X657C
